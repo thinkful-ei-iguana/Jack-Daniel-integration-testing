becfb4aa9d5edee6e946058ac19e59f445623d9d
Daniel Kent <Kentpercussion@gmail.com>
2019-12-04 10:23:35 -0700

    added sorting tests

--- index.js
+++ index.js
@@ -6,23 +6,32 @@ const app = express();
 app.get('/apps', (req, res) => {
   const validFilters = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
   let filteredApps = apps;
-  if (validFilters.includes(req.query.Genres)) {
-    console.log(req.query);
-    const genre = req.query.Genres.toLowerCase();
-    filteredApps = apps.filter(app => {
-      return app.Genres.split(';')[0].toLowerCase() === genre;
-    });
-  }
-  const validSorts = ['Rating', 'App'];
-  if (validSorts.includes(req.query.sort)) {
-    const sort = req.query.sort;
-    filteredApps = filteredApps.sort((app1, app2) => 
-      app1[sort] > app2[sort] ? 1 : -1
-    );
-  }
 
-  
+  if (req.query.filter) {
+    if (validFilters.includes(req.query.Genres)) {
+      console.log(req.query);
+      const genre = req.query.Genres.toLowerCase();
+      filteredApps = apps.filter(app => {
+        return app.Genres.split(';')[0].toLowerCase() === genre;
+      });
+    } else {
+      res.status(400);
+      res.send('filter is invalid');
+    }
+  }
+  const validSorts = ['rating', 'app'];
+  if (req.query.sort) {
+    if (validSorts.includes(req.query.sort)) {
+      const sort = req.query.sort;
+      filteredApps = filteredApps.sort((app1, app2) => 
+        app1[sort] > app2[sort] ? 1 : -1
+      );
+    } else {
+      res.status(400);
+      res.send('sort is invalid');
+    }
+  }
   res.send(filteredApps);
 });
 
-app.listen(8000);
+module.exports = app;
---
+++ test/app.test.js
@@ -0,0 +1,43 @@
+const {expect} = require('chai');
+const supertest = require('supertest');
+const app = require('../index');
+
+describe('app get request', () => {
+  it('should return 200 status if /apps endpoint', () => {
+    return supertest(app).get('/apps').expect(200);
+  });
+  it('should return 404 for any other endpoint', () => {
+    return supertest(app).get('/poop').expect(404);
+  });
+
+  it('should return an array of app objects', () => {
+    return supertest(app).get('/apps')
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf.at.least(1);
+        const app = res.body[0];
+        expect(app).to.include.all.keys(
+          'App', 'Category', 'Rating'
+        );
+      });
+  });
+
+  it('should be 400 if sort is incorrect', () => {
+    return supertest(app).get('/apps').query({sort: 'lol'}).expect(400);
+  });
+
+  it('should sort apps in accordance to selected parameter', () => {
+    const validSorts = ['rating', 'app'];
+    validSorts.forEach(sort => {
+      return supertest(app).get('/apps').query({sort: sort})
+        .then(res => {
+          for (let i=0; i <= res.body.length-2; ++i){
+            const app1 = res.body[i];
+            const app2 = res.body[i+1];
+            expect(app1[sort] <= app2[sort]);
+          }
+        });
+    });
+  });
+});
